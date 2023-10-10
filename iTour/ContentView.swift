7bf54d9537600b9dafa1b678f0cafc13a0c6ab2a
Rob Ranf <rob@emiyaconsulting.com>
2023-10-10 10:57:16 -0700

    Initial Commit

---
+++ iTour/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iTour
+//
+//  Created by Rob Ranf on 10/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
