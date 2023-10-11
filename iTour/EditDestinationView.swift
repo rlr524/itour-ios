aa9a5fc3fca02515dbc1768825fd23271fe05fa2
Rob Ranf <rob@emiyaconsulting.com>
2023-10-10 18:19:39 -0700

    Adding handler methods for get and delete

---
+++ iTour/EditDestinationView.swift
@@ -0,0 +1,18 @@
+//
+//  EditDestinationView.swift
+//  iTour
+//
+//  Created by Rob Ranf on 10/10/23.
+//
+
+import SwiftUI
+
+struct EditDestinationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditDestinationView()
+}
