ebbcd0c35039dd6e02ea1eaebf4ea0f32dc30ad9
Rob Ranf <rob@emiyaconsulting.com>
2023-10-11 18:17:41 -0700

    Adding add destination and custom sorting

--- iTour/Views/DestinationListingView.swift
+++ iTour/Views/DestinationListingView.swift
@@ -1,18 +1,18 @@
 //
-//  EditDestinationView.swift
+//  DestinationListingView.swift
 //  iTour
 //
-//  Created by Rob Ranf on 10/10/23.
+//  Created by Rob Ranf on 10/11/23.
 //
 
 import SwiftUI
 
-struct EditDestinationView: View {
+struct DestinationListingView: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
 #Preview {
-    EditDestinationView()
+    DestinationListingView()
 }
